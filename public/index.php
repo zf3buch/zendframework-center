c35a8487a6a363206d3be32e9b192bb7fac4af6f
Ralf Eggert <ralf@travello.com>
2016-06-04 17:34:25 +0200

    Added support for builtin webserver

--- public/index.php
+++ public/index.php
@@ -20,6 +20,15 @@ define(
     : 'production')
 );
 
+// Decline static file requests back to the PHP built-in webserver
+if (php_sapi_name() === 'cli-server') {
+    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+    if (__FILE__ !== $path && is_file($path)) {
+        return false;
+    }
+    unset($path);
+}
+
 // setup autoloading from composer
 require_once PROJECT_ROOT . '/vendor/autoload.php';
 
