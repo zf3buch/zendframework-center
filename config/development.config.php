3079becfc085c3d8b591efd2d61afece7277bf4d
Ralf Eggert <ralf@travello.com>
2016-06-30 09:46:06 +0200

    Refactored application config files

--- config/development.config.php
+++ config/development.config.php
@@ -8,27 +8,18 @@
  */
 
 return [
-    'modules'                 => [
+    'modules' => [
         'ZendDeveloperTools',
         'SanSessionToolbar',
-        'Zend\Session',
-        'Zend\Router',
-        'Zend\Validator',
-        'Application',
     ],
+
     'module_listener_options' => [
-        'config_glob_paths'        => [
+        'config_glob_paths' => [
             PROJECT_ROOT
             . '/config/autoload/{,*.}{global,development,local}.php',
         ],
-        'module_paths'             => [
-            PROJECT_ROOT . '/module',
-            PROJECT_ROOT . '/vendor',
-        ],
-        'cache_dir'                => PROJECT_ROOT . '/data/cache',
+
         'config_cache_enabled'     => false,
-        'config_cache_key'         => 'module_config_cache',
         'module_map_cache_enabled' => false,
-        'module_map_cache_key'     => 'module_map_cache',
     ],
 ];
