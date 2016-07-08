781cb8d77a4b65b40023886940655424180e4c57
Ralf Eggert <ralf@travello.com>
2016-07-08 18:36:34 +0200

    Added Logout form

---
+++ module/UserFrontend/src/Form/UserLogoutFormInterface.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * ZF3 book Zend Framework Center Example Application
+ *
+ * @author     Ralf Eggert <ralf@travello.de>
+ * @link       https://github.com/zf3buch/zendframework-center
+ * @license    http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace UserFrontend\Form;
+
+use Zend\Form\FormInterface;
+
+/**
+ * Interface UserLogoutFormInterface
+ *
+ * @package UserFrontend\Form
+ */
+interface UserLogoutFormInterface extends FormInterface
+{
+}
