fe46b40bd124d6a1412af0c0dd328f1c516733bc
Ralf Eggert <ralf@travello.com>
2016-07-09 10:50:58 +0200

    Added forbidden page

--- module/UserFrontend/language/en_US.php
+++ module/UserFrontend/language/en_US.php
@@ -9,39 +9,43 @@
 
 return [
     // navigation texts
-    'user_frontend_navigation_index'     => 'User',
+    'user_frontend_navigation_index'      => 'User',
 
     // title texts
-    'user_frontend_title_index_index'    => 'User',
-    'user_frontend_title_login_index'    => 'Login user',
-    'user_frontend_title_register_index' => 'Register user',
-    'user_frontend_title_edit_index'     => 'Edit user',
+    'user_frontend_title_index_index'     => 'User',
+    'user_frontend_title_login_index'     => 'Login user',
+    'user_frontend_title_forbidden_index' => 'Access forbidden',
+    'user_frontend_title_register_index'  => 'Register user',
+    'user_frontend_title_edit_index'      => 'Edit user',
 
     // heading texts
-    'user_frontend_h1_index_index'       => 'User',
-    'user_frontend_h1_login_index'       => 'Login user',
-    'user_frontend_h1_register_index'    => 'Register user',
-    'user_frontend_h1_edit_index'        => 'Edit user',
-    'user_frontend_h2_login'             => 'Login user',
-    'user_frontend_h2_register'          => 'Register user',
-    'user_frontend_h2_edit'              => 'Edit user',
+    'user_frontend_h1_index_index'        => 'User',
+    'user_frontend_h1_login_index'        => 'Login user',
+    'user_frontend_h1_forbidden_index'    => 'Access forbidden',
+    'user_frontend_h1_register_index'     => 'Register user',
+    'user_frontend_h1_edit_index'         => 'Edit user',
+    'user_frontend_h2_login'              => 'Login user',
+    'user_frontend_h2_forbidden'          => 'Access not allowed',
+    'user_frontend_h2_register'           => 'Register user',
+    'user_frontend_h2_edit'               => 'Edit user',
 
     // message texts
-    'user_frontend_message_register'     => 'If you are not registered yet, you can register now!',
-    'user_frontend_message_registered'   => 'You registered successfully and can login now!',
-    'user_frontend_message_login'        => 'If you are already registered, you can login now!',
-    'user_frontend_message_edit'         => 'You can edit the user data now!',
-    'user_frontend_message_check_data'   => 'Please check the user data!',
-    'user_frontend_message_form_timeout' => 'Timeout! Please send the form again!',
+    'user_frontend_message_register'      => 'If you are not registered yet, you can register now!',
+    'user_frontend_message_registered'    => 'You registered successfully and can login now!',
+    'user_frontend_message_login'         => 'If you are already registered, you can login now!',
+    'user_frontend_message_forbidden'     => 'You are not allowed to access this page! You can logout now and login again with another user account with sufficient access right!',
+    'user_frontend_message_edit'          => 'You can edit the user data now!',
+    'user_frontend_message_check_data'    => 'Please check the user data!',
+    'user_frontend_message_form_timeout'  => 'Timeout! Please send the form again!',
 
     // label texts
-    'user_frontend_label_email'          => 'Email',
-    'user_frontend_label_password'       => 'Password',
+    'user_frontend_label_email'           => 'Email',
+    'user_frontend_label_password'        => 'Password',
 
     // action texts
-    'user_frontend_action_login'         => 'Login user',
-    'user_frontend_action_logout'        => 'Logout user',
-    'user_frontend_action_register'      => 'Register user',
-    'user_frontend_action_edit'          => 'Edit user',
-    'user_frontend_action_cancel'        => 'Cancel',
+    'user_frontend_action_login'          => 'Login user',
+    'user_frontend_action_logout'         => 'Logout user',
+    'user_frontend_action_register'       => 'Register user',
+    'user_frontend_action_edit'           => 'Edit user',
+    'user_frontend_action_cancel'         => 'Cancel',
 ];
