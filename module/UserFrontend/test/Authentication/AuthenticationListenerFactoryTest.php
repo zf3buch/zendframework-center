ded00138dd54c882b46824973ea3b7420c5531aa
Ralf Eggert <ralf@travello.com>
2016-07-19 15:22:57 +0200

    Added test for ViewListener

---
+++ module/UserFrontend/test/Authentication/AuthenticationListenerFactoryTest.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * ZF3 book Zend Framework Center Example Application
+ *
+ * @author     Ralf Eggert <ralf@travello.de>
+ * @link       https://github.com/zf3buch/zendframework-center
+ * @license    http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace ApplicationTest\I18n;
+
+use Application\I18n\I18nListener;
+use Application\I18n\I18nListenerFactory;
+use Interop\Container\ContainerInterface;
+use PHPUnit_Framework_TestCase;
+use Prophecy\Prophecy\MethodProphecy;
+use Zend\I18n\Translator\TranslatorInterface;
+
+/**
+ * Class I18nListenerFactoryTest
+ *
+ * @package ApplicationTest\I18n
+ */
+class I18nListenerFactoryTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Test factory
+     */
+    public function testFactory()
+    {
+        $config = [
+            'i18n' => [
+                'defaultLang'    => 'de',
+                'allowedLocales' => [
+                    'de' => 'de_DE',
+                    'en' => 'en_US',
+                ],
+            ],
+        ];
+
+        /** @var ContainerInterface $container */
+        $container = $this->prophesize(ContainerInterface::class);
+
+        /** @var TranslatorInterface $translator */
+        $translator = $this->prophesize(TranslatorInterface::class);
+
+        /** @var MethodProphecy $method */
+        $method = $container->get(TranslatorInterface::class);
+        $method->willReturn($translator);
+        $method->shouldBeCalled();
+
+        /** @var MethodProphecy $method */
+        $method = $container->get('config');
+        $method->willReturn($config);
+        $method->shouldBeCalled();
+
+        $factory = new I18nListenerFactory();
+
+        $this->assertTrue(
+            $factory instanceof I18nListenerFactory
+        );
+
+        /** @var I18nListener $table */
+        $listener = $factory($container->reveal(), I18nListener::class);
+
+        $this->assertTrue($listener instanceof I18nListener);
+
+        $this->assertAttributeEquals(
+            $translator->reveal(), 'translator', $listener
+        );
+        $this->assertAttributeEquals(
+            $config['i18n']['defaultLang'], 'defaultLang', $listener
+        );
+        $this->assertAttributeEquals(
+            $config['i18n']['allowedLocales'], 'allowedLocales', $listener
+        );
+    }
+}
---
+++ module/UserFrontend/test/Authentication/AuthenticationListenerTest.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * ZF3 book Zend Framework Center Example Application
+ *
+ * @author     Ralf Eggert <ralf@travello.de>
+ * @link       https://github.com/zf3buch/zendframework-center
+ * @license    http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace ApplicationTest\I18n;
+
+use Application\I18n\I18nListener;
+use Locale;
+use PHPUnit_Framework_TestCase;
+use Prophecy\Prophecy\MethodProphecy;
+use Zend\EventManager\EventManagerInterface;
+use Zend\Http\Headers;
+use Zend\Http\PhpEnvironment\Request;
+use Zend\Http\PhpEnvironment\Response;
+use Zend\I18n\Translator\Translator;
+use Zend\Mvc\MvcEvent;
+use Zend\Router\RouteMatch;
+
+/**
+ * Class I18nListenerTest
+ *
+ * @package ApplicationTest\I18n
+ */
+class I18nListenerTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var I18nListener
+     */
+    private $i18nListener;
+
+    /**
+     * @var Translator
+     */
+    private $translator;
+
+    /**
+     * Setup test cases
+     */
+    protected function setUp()
+    {
+        $this->translator = $this->prophesize(Translator::class);
+
+        $this->i18nListener = new I18nListener();
+        $this->i18nListener->setTranslator($this->translator->reveal());
+        $this->i18nListener->setDefaultLang('de');
+        $this->i18nListener->setAllowedLocales(
+            [
+                'de' => 'de_DE',
+                'en' => 'en_US',
+            ]
+        );
+    }
+
+    /**
+     * Test attaching listeners
+     */
+    public function testAttach()
+    {
+        $events = $this->prophesize(EventManagerInterface::class);
+
+        /** @var MethodProphecy $method */
+        $method = $events->attach(
+            MvcEvent::EVENT_ROUTE,
+            [$this->i18nListener, 'redirectHomeRoute'],
+            100
+        );
+        $method->willReturn([$this->i18nListener, 'redirectHomeRoute']);
+        $method->shouldBeCalled();
+
+        /** @var MethodProphecy $method */
+        $method = $events->attach(
+            MvcEvent::EVENT_ROUTE,
+            [$this->i18nListener, 'setupLocalization'],
+            -100
+        );
+        $method->willReturn([$this->i18nListener, 'redirectHomeRoute']);
+        $method->shouldBeCalled();
+
+        $this->i18nListener->attach($events->reveal());
+    }
+
+    /**
+     * Test redirect to home route with request uri /whatever
+     */
+    public function testRedirectHomeRouteWhatever()
+    {
+        $request = $this->prophesize(Request::class);
+
+        /** @var MethodProphecy $method */
+        $method = $request->getRequestUri();
+        $method->willReturn('/whatever');
+        $method->shouldBeCalled();
+
+        $response = $this->prophesize(Response::class);
+
+        /** @var MethodProphecy $method */
+        $method = $response->setStatusCode();
+        $method->shouldNotBeCalled();
+
+        $mvcEvent = $this->prophesize(MvcEvent::class);
+
+        /** @var MethodProphecy $method */
+        $method = $mvcEvent->getRequest();
+        $method->willReturn($request);
+        $method->shouldBeCalled();
+
+        $this->i18nListener->redirectHomeRoute($mvcEvent->reveal());
+    }
+
+    /**
+     * Test redirect to home route with request uri /
+     */
+    public function testRedirectHomeRouteRoot()
+    {
+        $request = $this->prophesize(Request::class);
+
+        /** @var MethodProphecy $method */
+        $method = $request->getRequestUri();
+        $method->willReturn('/');
+        $method->shouldBeCalled();
+
+        $headers = $this->prophesize(Headers::class);
+
+        /** @var MethodProphecy $method */
+        $method = $headers->addHeaderLine('Location', '/de');
+        $method->shouldBeCalled();
+
+        $response = $this->prophesize(Response::class);
+
+        /** @var MethodProphecy $method */
+        $method = $response->getHeaders();
+        $method->willReturn($headers);
+        $method->shouldBeCalled();
+
+        /** @var MethodProphecy $method */
+        $method = $response->setStatusCode(Response::STATUS_CODE_301);
+        $method->shouldBeCalled();
+
+        $mvcEvent = $this->prophesize(MvcEvent::class);
+
+        /** @var MethodProphecy $method */
+        $method = $mvcEvent->getRequest();
+        $method->willReturn($request);
+        $method->shouldBeCalled();
+
+        /** @var MethodProphecy $method */
+        $method = $mvcEvent->getResponse();
+        $method->willReturn($response);
+        $method->shouldBeCalled();
+
+        $this->i18nListener->redirectHomeRoute($mvcEvent->reveal());
+    }
+
+    /**
+     * Test setup the localization
+     *
+     * @param string $locale
+     * @param string $lang
+     *
+     * @dataProvider provideSetupLocalization
+     */
+    public function testSetupLocalization($locale, $lang)
+    {
+        /** @var MethodProphecy $method */
+        $method = $this->translator->setLocale($locale);
+        $method->shouldBeCalled();
+
+        $routeMatch = $this->prophesize(RouteMatch::class);
+
+        /** @var MethodProphecy $method */
+        $method = $routeMatch->getParam('lang', 'de');
+        $method->willReturn($lang);
+        $method->shouldBeCalled();
+
+        $mvcEvent = $this->prophesize(MvcEvent::class);
+
+        /** @var MethodProphecy $method */
+        $method = $mvcEvent->getRouteMatch();
+        $method->willReturn($routeMatch);
+        $method->shouldBeCalled();
+
+        $this->i18nListener->setupLocalization($mvcEvent->reveal());
+
+        $this->assertEquals($locale, Locale::getDefault());
+    }
+
+    /**
+     * Data provider for setup the localization
+     *
+     * @return array
+     */
+    public function provideSetupLocalization()
+    {
+        return [
+            ['de_DE', 'de'],
+            ['en_US', 'en'],
+        ];
+    }
+}
