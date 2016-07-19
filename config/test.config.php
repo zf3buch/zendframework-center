f53c1389bfa7ebe65c85f42ac747a871a138050c
Ralf Eggert <ralf@travello.com>
2016-07-19 22:03:56 +0200

    Added first controller test

---
+++ config/test.config.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * ZF3 book Zend Framework Center Example Application
+ *
+ * @author     Ralf Eggert <ralf@travello.de>
+ * @link       https://github.com/zf3buch/zendframework-center
+ * @license    http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+return [
+    'modules'                 => [
+        'Zend\Mvc\Plugin\Identity',
+        'TravelloFilter',
+        'TravelloViewHelper',
+        'Zend\Mvc\I18n',
+        'Zend\Mvc\Plugin\FlashMessenger',
+        'Zend\I18n',
+        'Zend\Form',
+        'Zend\InputFilter',
+        'Zend\Db',
+        'Zend\Filter',
+        'Zend\Hydrator',
+        'Zend\Paginator',
+        'Zend\Navigation',
+        'Zend\Session',
+        'Zend\Router',
+        'Zend\Validator',
+        'UserFrontend',
+        'UserBackend',
+        'UserModel',
+        'CompanyBackend',
+        'CompanyModel',
+        'AdvertFrontend',
+        'AdvertBackend',
+        'AdvertModel',
+        'Application',
+    ],
+    'module_listener_options' => [
+        'config_glob_paths' => [
+            PROJECT_ROOT
+            . '/config/autoload/{,*.}{global,test,local}.php',
+        ],
+        'module_paths'             => [
+            PROJECT_ROOT . '/module',
+            PROJECT_ROOT . '/vendor',
+        ],
+        'cache_dir'                => PROJECT_ROOT . '/data/cache',
+        'config_cache_enabled'     => false,
+        'config_cache_key'         => 'application.config.cache',
+        'module_map_cache_enabled' => false,
+        'module_map_cache_key'     => 'application.module.cache',
+    ],
+];
--- module/Application/test/Controller/IndexControllerTest.php
+++ module/Application/test/Controller/IndexControllerTest.php
@@ -9,37 +9,161 @@
 
 namespace ApplicationTest\Controller;
 
-use Zend\Test\PHPUnit\Controller\AbstractControllerTestCase;
+use Application\Controller\IndexController;
+use PHPUnit_Extensions_Database_DataSet_CsvDataSet;
+use PHPUnit_Extensions_Database_DataSet_IDataSet;
+use PHPUnit_Extensions_Database_DB_DefaultDatabaseConnection;
+use PHPUnit_Extensions_Database_DB_IDatabaseConnection;
+use PHPUnit_Extensions_Database_TestCase_Trait;
+use Zend\Db\Adapter\Adapter;
+use Zend\I18n\Translator\TranslatorInterface;
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
 
 /**
  * Class IndexControllerTest
  *
  * @package ApplicationTest\Controller
  */
-class IndexControllerTest extends AbstractControllerTestCase
+class IndexControllerTest extends AbstractHttpControllerTestCase
 {
+    use PHPUnit_Extensions_Database_TestCase_Trait;
+
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    /**
+     * @var Adapter
+     */
+    private $adapter = null;
+
+    /**
+     * @var PHPUnit_Extensions_Database_DB_DefaultDatabaseConnection
+     */
+    private $connection = null;
+
+    /**
+     * Returns the test database connection.
+     *
+     * @return PHPUnit_Extensions_Database_DB_IDatabaseConnection
+     */
+    protected function getConnection()
+    {
+        if (!$this->connection) {
+            $this->connection = $this->createDefaultDBConnection(
+                $this->adapter->getDriver()->getConnection()->getResource(
+                ),
+                'zf-center-test'
+            );
+        }
+
+        return $this->connection;
+    }
+
+    /**
+     * Returns the test dataset.
+     *
+     * @return PHPUnit_Extensions_Database_DataSet_IDataSet
+     */
+    protected function getDataSet()
+    {
+        $dataSet = new PHPUnit_Extensions_Database_DataSet_CsvDataSet();
+        $dataSet->addTable(
+            'company',
+            PROJECT_ROOT . "/data/test-data/company.test-data.csv"
+        );
+        $dataSet->addTable(
+            'advert',
+            PROJECT_ROOT . "/data/test-data/advert.homepage.test-data.csv"
+        );
+
+        return $dataSet;
+    }
+
     /**
      * Setup test cases
      */
     public function setUp()
     {
-        $this->setApplicationConfig(
-            include PROJECT_ROOT . '/config/application.config.php'
-        );
         parent::setUp();
+
+        $this->setApplicationConfig(
+            include PROJECT_ROOT . '/config/test.config.php'
+        );
+
+        $this->translator = $this->getApplicationServiceLocator()->get(
+            TranslatorInterface::class
+        );
+
+        $this->adapter = $this->getApplicationServiceLocator()->get(
+            Adapter::class
+        );
+
+        $this->databaseTester = null;
+
+        $this->getDatabaseTester()->setSetUpOperation(
+            $this->getSetUpOperation()
+        );
+        $this->getDatabaseTester()->setDataSet($this->getDataSet());
+        $this->getDatabaseTester()->onSetUp();
     }
 
     /**
-     * Test index action
+     * Test index action without lang
      */
-    public function testIndexActionCanBeAccessed()
+    public function testIndexActionWithoutLangCannotBeAccessed()
     {
-        $this->dispatch('/');
+        $this->dispatch('/', 'GET');
+        $this->assertResponseStatusCode(301);
+    }
+
+    /**
+     * Test index action with lang
+     */
+    public function testIndexActionWithLangCanBeAccessed()
+    {
+        $this->dispatch('/de', 'GET');
         $this->assertResponseStatusCode(200);
 
-        $this->assertModuleName('application');
-        $this->assertControllerName('application_index');
-        $this->assertControllerClass('IndexController');
         $this->assertMatchedRouteName('home');
+        $this->assertModuleName('application');
+        $this->assertControllerName(IndexController::class);
+        $this->assertControllerClass('IndexController');
+        $this->assertActionName('index');
+    }
+
+    /**
+     * Test index action output
+     */
+    public function testIndexActionOutput()
+    {
+        $this->dispatch('/de', 'GET');
+
+        $this->assertQuery('.page-header h1');
+        $this->assertQueryContentContains(
+            '.page-header h1',
+            $this->translator->translate('application_h1_index_index')
+        );
+
+        $queryJob = $this->getConnection()->createQueryTable(
+            'fetchJob',
+            'SELECT * FROM advert WHERE id = "1";'
+        );
+
+        $this->assertQueryContentContains(
+            '.panel-primary .panel-heading strong a',
+            $queryJob->getRow(0)['title']
+        );
+
+        $queryProject = $this->getConnection()->createQueryTable(
+            'fetchProject',
+            'SELECT * FROM advert WHERE id = "3";'
+        );
+
+        $this->assertQueryContentContains(
+            '.panel-success .panel-heading strong a',
+            $queryProject->getRow(0)['title']
+        );
     }
 }
